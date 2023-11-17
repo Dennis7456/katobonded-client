74795cecbae9846488176f68d4f0f3f312486912
Dennis Kiplangat <denniskiplangat.dk@gmail.com>
2023-11-17 16:03:15 +0300

    build: gitHub action setup

--- src/stories/CreateYourTrip/CreateYourTrip.stories.ts
+++ src/stories/CreateYourTrip/CreateYourTrip.stories.ts
@@ -2,20 +2,17 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import CreateYourTrip from './CreateYourTrip'
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof CreateYourTrip> = {
   title: 'Example/CreateYourTrip',
   component: CreateYourTrip,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'full',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
 }
 
 export default meta
+
 type Story = StoryObj<typeof meta>
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Default: Story = {}
