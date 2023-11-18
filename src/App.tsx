c42a21410a936562e08607ec37d5fafae09916e3
Dennis Kiplangat <denniskiplangat.dk@gmail.com>
2023-11-18 23:23:45 +0300

    build: added places api configuration to input on create your trip

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import NavigationBar from './components/Header/NavigationBar'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { Default } from './Default'
 
 const App: React.FC = () => {
   return (
     <div>
-      <NavigationBar />
-      <FontAwesomeIcon icon={icon({ name: 'user', style: 'regular' })} />
+      <Default />
     </div>
   )
 }
